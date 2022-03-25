eee5ac0934c09cdd5d98e8b782caa2b2408822a6
Agustin Bollati <bollatiagustin@gmail.com>
2022-03-24 23:53:25 -0300

    Agrego guards para proteger las rutas de login y gifs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
+import { AngularFireAuthGuard, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
+
+const redirectLoggedInToGifs = () => redirectLoggedInTo(['gifs']);
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [AngularFireAuthGuard],
+    data: {
+      authGuardPipe: redirectLoggedInToGifs
+    }
+  },
+  {
+    path: 'gifs',
+    loadChildren: () => import('./gifs/gifs.module').then(m => m.GifsModule),
+    canActivate: [AngularFireAuthGuard],
+    data: {
+      authGuardPipe: redirectUnauthorizedToLogin
+    }
   },
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'gifs',
     pathMatch: 'full'
   },
   {
     path: '**',
-    redirectTo: 'login'
+    redirectTo: 'gifs'
   }
 ];
 
