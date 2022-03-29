5377da6f586b457e742a83b9a81fa11bf410d1f1
Agustin Bollati <bollatiagustin@gmail.com>
2022-03-29 17:21:21 -0300

    Agrego cambios en caso de error al no obtener el detalle

--- src/app/gifs/pages/gif-detail/gif-detail.component.ts
+++ src/app/gifs/pages/gif-detail/gif-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Data } from '@angular/router';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { ActivatedRoute, Data, Router } from '@angular/router';
 
 import { GIF } from '../../interfaces/gif.interface';
 
@@ -8,19 +9,32 @@ import { GIF } from '../../interfaces/gif.interface';
   templateUrl: './gif-detail.component.html',
   styleUrls: ['./gif-detail.component.scss']
 })
-export class GifDetailComponent implements OnInit {
+export class GifDetailComponent implements OnInit, AfterViewInit {
+  @ViewChild('favoriteIcon') favoriteIcon: MatIcon;
   gif: GIF;
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.route.data.subscribe({
       next: (value: Data) => {
         this.gif = value['gif'];
+        if (value['gif'] && value['gif'].meta.status === 404) {
+          this.router.navigate(['/gifs/list']);
+
+          return;
+        }
       }
     });
+  }
+
+  ngAfterViewInit() {
+    // this.favoriteIcon._elementRef.nativeElement.innerHTML = 'search';
+    // console.log(this.favoriteIcon);
   }
 
 }
--- src/app/gifs/pages/gif-detail/gif-detail.resolver.ts
+++ src/app/gifs/pages/gif-detail/gif-detail.resolver.ts
@@ -1,18 +1,32 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
 
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
+import { HttpResponse } from '../../../shared/interfaces/http-response.interface';
 
 import { GIF } from '../../interfaces/gif.interface';
 
 import { GifService } from '../../services/gif.service';
 
 @Injectable({ providedIn: 'root' })
-export class GifDetailResolver implements Resolve<GIF> {
-  constructor(private gifService: GifService) {
+export class GifDetailResolver implements Resolve<GIF | any> {
+  constructor(
+    private router: Router,
+    private gifService: GifService
+  ) {
   }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<GIF> | Promise<GIF> | GIF {
-    return this.gifService.getById(route.params['id']).pipe(map((res) => res.data));
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<GIF> | Observable<any> {
+    console.log((route.params['id']));
+
+    return this.gifService.getById(route.params['id']).pipe(
+      map((res) => {
+        return res.data;
+      }),
+      catchError((err: HttpErrorResponse) => {
+        return of(err.error);
+      })
+    );
   }
 }
